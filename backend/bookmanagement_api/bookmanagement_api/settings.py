9bf1fe5a42f32d81348cb5229e30ee10e7ddc0b3
Raijo Cheruvathoor <raijocheruvathoor@gmail.com>
2025-06-14 11:25:33 +0530

    added cores allowed origins

--- backend/bookmanagement_api/bookmanagement_api/settings.py
+++ backend/bookmanagement_api/bookmanagement_api/settings.py
@@ -173,7 +173,7 @@ STATIC_ROOT = BASE_DIR / "static"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = ['https://bookmanagement-flame.vercel.app']
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days = 1),
