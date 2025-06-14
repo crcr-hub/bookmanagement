42785cc31cf8382a293656b69bb7bdbf35815562
Raijo Cheruvathoor <raijocheruvathoor@gmail.com>
2025-06-14 14:12:17 +0530

    cahnges in database

--- backend/bookmanagement_api/bookmanagement_api/settings.py
+++ backend/bookmanagement_api/bookmanagement_api/settings.py
@@ -98,26 +98,26 @@ WSGI_APPLICATION = 'bookmanagement_api.wsgi.application'
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bookmanagement',
-        'USER' : 'postgres',
-        'PASSWORD' : 'raijo',
-        'HOST' : 'localhost',
-        'PORT' : '5432'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'bookmanagement',
+#         'USER' : 'postgres',
+#         'PASSWORD' : 'raijo',
+#         'HOST' : 'localhost',
+#         'PORT' : '5432'
+#     }
+# }
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
