292165c5a5deb9bdde9ad8f1a2fafc6f570c96be
Raijo Cheruvathoor <raijocheruvathoor@gmail.com>
2025-06-14 10:21:03 +0530

    Updated urls

--- backend/bookmanagement_api/books/urls.py
+++ backend/bookmanagement_api/books/urls.py
@@ -19,7 +19,6 @@ urlpatterns = [
     path('',views.getRoutes,name='routes'),
     path('token/',CustomTokenObtainPairView.as_view(),name='token'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/',Registeriew.as_view(),name='register'),
     path('logout/',LogoutView.as_view(),name='logout'),
     path('user_details/',UserDetails.as_view(),name='user_details'),
